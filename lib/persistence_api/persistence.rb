be6c292fd218c1869c6e59b28b3d7676470a8d3a
Emilio Gutter <egutter@10pines.com>
2013-01-15 20:03:48 -0300

    loads memory implementation based on env variable

--- lib/persistence_api/persistence.rb
+++ lib/persistence_api/persistence.rb
@@ -1,13 +1,19 @@
 module Persistence
 
   def self.included(klass)
-    klass.send :include, persitence_strategy
+    klass.send :include, persistence_strategy
   end
 
-  def self.persitence_strategy
-    if ::Rails.env == 'test'
+  def self.persistence_strategy
+    if ENV['INHERIT_AR'] == 'memory'
+      require 'active_support/core_ext/string/inflections'
+      require File.dirname(__FILE__) + '/memory/memory_persistence'
+      require File.dirname(__FILE__) + '/memory/relation'
+      require File.dirname(__FILE__) + '/memory/condition'
       Persistence::Memory
     else
+      require 'active_record'
+      require File.dirname(__FILE__) +  '/rails/rails_persistence'
       Persistence::Rails
     end
   end
